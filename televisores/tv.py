4228ec3ac1d7e14b78a4108bd7bbcfbd521e10b7
Aroserot <aroserot@unal.edu.co>
2024-02-27 11:13:23 -0500

    cambios 7

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,46 +1,5 @@
 from __future__ import annotations
 from televisores.marca import Marca
-from typing import Type
-
-class Control:
-    def __init__(self) -> None:
-        self._tv = None
-
-    def enlazar(self, tv: TV):
-        self._tv = tv
-        tv.setControl(self)
-
-    def turnOn(self) -> None:
-        self._tv.turnOn()
-    def turnOff(self) -> None:
-        self._tv.turnOff()
-
-    def canalUp(self) -> None:
-        self._tv.canalUp()
-
-    def canalDown(self) -> None:
-        self._tv.canalDown()
-
-    def volumenUp(self) -> None:
-        self._tv.volumenUp()
-
-    def volumenDown(self) -> None:
-        self._tv.volumenDown()
-
-    def setCanal(self, canal: int) -> None:
-        self._tv.setCanal(canal)
-
-    def setVolumen(self, volumen: int) -> None:
-        self._tv.setVolumen(volumen)
-
-    def getTv(self) -> TV:
-        return self._tv
-    
-    def setTv(self, tv: TV) -> None:
-        self._tv = tv
-
-from __future__ import annotations
-from televisores.marca import Marca
 from televisores.control import Control
 
 class TV:
